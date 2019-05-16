50f668afcbef49e135ae3aac72a98a369254a4ca
Kylla Harris <kylla.harris@gmail.com>
2019-05-16 05:44:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    return false 
+    return true 
   else 
-    return true  
+    return false   
   end 
 end
